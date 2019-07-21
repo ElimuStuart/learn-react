ef04bf88a3a4bb5f772e3fa2e6f942df5e1735ae
ElimuStuart <stuartelimu@gmail.com>
2019-07-21 21:30:38 +0300

    created a functional component

---
+++ Functional Components/index.js
@@ -0,0 +1,12 @@
+
+const MyApp = () => {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+ReactDOM.render(<MyApp />, document.querySelector("#root"))
