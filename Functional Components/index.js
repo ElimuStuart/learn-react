3d132287a51adc6a3f54ab573204f706d6a3aa2e
ElimuStuart <stuartelimu@gmail.com>
2019-07-21 21:36:17 +0300

    functional components practice

--- Functional Components/index.js
+++ Functional Components/index.js
@@ -1,12 +1,28 @@
 
-const MyApp = () => {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    );
-}
+// const MyApp = () => {
+//     return (
+//         <ul>
+//             <li>1</li>
+//             <li>2</li>
+//             <li>3</li>
+//         </ul>
+//     );
+// }
 
-ReactDOM.render(<MyApp />, document.querySelector("#root"))
+// ReactDOM.render(<MyApp />, document.querySelector("#root"))
+
+const MyInfo = () => {
+    return (
+        <div>
+            <p>My name is Stuart Elimu</p>
+            <p>I like soccer + music, I'd like to have my vaccations in the following cities:</p>
+            <ul>
+                <li>Jinja</li>
+                <li>Kigali</li>
+                <li>Nairobi</li>
+            </ul>
+        </div>
+    )
+};
+
+ReactDOM.render(<MyInfo />, document.querySelector("#root"));
